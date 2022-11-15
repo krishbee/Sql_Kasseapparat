0a7b2bd8024b9e18906ca09c1d8edc528cb2ca96
Peter Immersen <71756091+Immer77@users.noreply.github.com>
2022-11-15 10:01:42 +0100

    commiting inserts

--- Inserts.sql
+++ Inserts.sql
@@ -1,79 +1,245 @@
+-- inserts --------------------------------------------------------------------------------
+insert into Product values ('Klosterbryg', 30, 10, 'Munkens foretrukne drik')
+insert into Product values ('Sweet Georgia Brown', 30, 10, 'Sød, rund og mørk')
+insert into Product values ('Extra Pilsner', 30, 10, 'Lidt mere af alt det gode')
+insert into Product values ('Celebration', 30, 10, 'Det skal fejres!')
+insert into Product values ('Blondie', 30, 10, 'Mindsker intelligensen')
+insert into Product values ('Forårsbryg', 30, 10, 'Så fuglene synger')
+insert into Product values ('India Pale Ale', 30, 10, 'Den blege inder')
+insert into Product values ('Julebryg', 30, 10, 'Jul på dåse')
+insert into Product values ('Juletønden', 30, 10, 'Jul i tønde')
+insert into Product values ('Old Strong Ale', 30, 10, 'Den vi glemte')
+insert into Product values ('Fregatten Jylland', 30, 10, 'aka båden Anna')
+insert into Product values ('Imperial Stout', 30, 10, 'En øl')
+insert into Product values ('Special', 30, 10, 'Mortens minde øl')
+insert into Product values ('Klosterbryg', 30, 10, 'Munkens foretrukne drik')
+insert into Product values ('Sweet Georgia Brown', 30, 10, 'Sød, rund og mørk')
+insert into Product values ('Extra Pilsner', 30, 10, 'Lidt mere af alt det gode')
+insert into Product values ('Celebration', 30, 10, 'Det skal fejres!')
+insert into Product values ('Blondie', 30, 10, 'Mindsker intelligensen')
+insert into Product values ('Forårsbryg', 30, 10, 'Så fuglene synger')
+insert into Product values ('India Pale Ale', 30, 10, 'Den blege inder')
+insert into Product values ('Julebryg', 30, 10, 'Jul på dåse')
+insert into Product values ('Juletønden', 30, 10, 'Jul i tønde')
+insert into Product values ('Old Strong Ale', 30, 10, 'Den vi glemte')
+insert into Product values ('Fregatten Jylland', 30, 10, 'aka båden Anna')
+insert into Product values ('Imperial Stout', 30, 10, 'En øl')
+insert into Product values ('Special', 30, 10, 'Mortens minde øl')
+insert into Product values ('Klosterbryg', 30, 10, 'Munkens foretrukne drik')
+insert into Product values ('Sweet Georgia Brown', 30, 10, 'Sød, rund og mørk')
+insert into Product values ('Extra Pilsner', 30, 10, 'Lidt mere af alt det gode')
+insert into Product values ('Celebration', 30, 10, 'Det skal fejres!')
+insert into Product values ('Blondie', 30, 10, 'Mindsker intelligensen')
+insert into Product values ('Forårsbryg', 30, 10, 'Så fuglene synger')
+insert into Product values ('India Pale Ale', 30, 10, 'Den blege inder')
+insert into Product values ('Julebryg', 30, 10, 'Jul på dåse')
+insert into Product values ('Juletønden', 30, 10, 'Jul i tønde')
+insert into Product values ('Old Strong Ale', 30, 10, 'Den vi glemte')
+insert into Product values ('Fregatten Jylland', 30, 10, 'aka båden Anna')
+insert into Product values ('Imperial Stout', 30, 10, 'En øl')
+insert into Product values ('Special', 30, 10, 'Mortens minde øl')
+insert into Product values ('Æblebrus', 30, 10, '')
+insert into Product values ('Chips', 30, 10, '')
+insert into Product values ('Peanuts', 30, 10, '')
+insert into Product values ('Cola', 30, 10, '')
+insert into Product values ('Nikoline', 30, 10, '')
+insert into Product values ('7-Up', 30, 10, '')
+insert into Product values ('Vand', 30, 10, '')
+insert into Product values ('Ølpølser', 30, 10, '')
+insert into Product values ('Whiskey 45%', 30, 10, '50cl rør')
+insert into Product values ('Whiskey 45%', 30, 10, '4 cl')
+insert into Product values ('Whiskey 43%', 30, 10, '50 cl rør')
+insert into Product values ('Whiskey 43%', 30, 10, ' u/ egesplint')
+insert into Product values ('Whiskey 43%', 30, 10, ' m/ egesplint')
+insert into Product values ('Whiskeyglas & Brikker', 30, 10, '2 stk')
+insert into Product values ('Liquer of Aarhus', 30, 10, '')
+insert into Product values ('Lyng gin', 30, 10, '50 cl')
+insert into Product values ('Lyng gin', 30, 10, '4 cl')
+insert into Product values ('Klosterbryg', 30, 10, '20 l')
+insert into Product values ('Jazz Classic', 30, 10, '25 l')
+insert into Product values ('Extra Pilsner', 30, 10, '25 l')
+insert into Product values ('Celebration', 30, 10, '20 l')
+insert into Product values ('Blondie', 30, 10, '25 l')
+insert into Product values ('Forårsbryg', 30, 10, '20 l')
+insert into Product values ('India Pale Ale', 30, 10, '20 l')
+insert into Product values ('Julebryg', 30, 10, '20 l')
+insert into Product values ('Imperial Stout', 30, 10, '20 l')
+insert into Product values ('Kulsyre - 6 kg', 30, 10, '')
+insert into Product values ('Kulsyre - 4 kg', 30, 10, '')
+insert into Product values ('Kulsyre - 10 kg', 30, 10, '')
+insert into Product values ('Malt', 30, 10, '25 kg')
+insert into Product values ('T-shirt', 30, 10, '')
+insert into Product values ('Polo', 30, 10, '')
+insert into Product values ('Cap', 30, 10, '')
+insert into Product values ('1 Hane', 30, 10, '')
+insert into Product values ('2 Haner', 30, 10, '')
+insert into Product values ('Bar med flere haner', 30, 10, '')
+insert into Product values ('Anlæg Levering', 30, 10, '')
+insert into Product values ('Krus', 30, 10, '')
+insert into Product values ('Glas', 30, 10, 'Uanset størrelse')
+insert into Product values ('Gaveæske', 30, 10, '2 øl, 2 glas')
+insert into Product values ('Gaveæske', 30, 10, '4 øl')
+insert into Product values ('Trækasse', 30, 10, '6 øl')
+insert into Product values ('Gavekurv', 30, 10, '6 øl, 2 glas')
+insert into Product values ('Trækasse', 30, 10, '6 øl, 6 glas')
+insert into Product values ('Trækasse', 30, 10, '12 øl')
+insert into Product values ('Papkasse', 30, 10, '12 øl')
+
+insert into ProductCategory values ('Flaskeøl', '')
+insert into ProductCategory values ('Fadøl', '40 cl')
+insert into ProductCategory values ('Snacks', '')
+insert into ProductCategory values ('Spiritus', '')
+insert into ProductCategory values ('Fustage', '')
+insert into ProductCategory values ('Kulsyre', '')
+insert into ProductCategory values ('Malt', '')
+insert into ProductCategory values ('Beklædning', '')
+insert into ProductCategory values ('Anlæg', '')
+insert into ProductCategory values ('Glas', '')
+insert into ProductCategory values ('Sampakninger', '')
+
+insert into ProductCategoryProduct values ('Flaskeøl', 1)
+insert into ProductCategoryProduct values ('Flaskeøl', 2)
+insert into ProductCategoryProduct values ('Flaskeøl', 3)
+insert into ProductCategoryProduct values ('Flaskeøl', 4)
+insert into ProductCategoryProduct values ('Flaskeøl', 5)
+insert into ProductCategoryProduct values ('Flaskeøl', 6)
+insert into ProductCategoryProduct values ('Flaskeøl', 7)
+insert into ProductCategoryProduct values ('Flaskeøl', 8)
+insert into ProductCategoryProduct values ('Flaskeøl', 9)
+insert into ProductCategoryProduct values ('Flaskeøl', 10)
+insert into ProductCategoryProduct values ('Flaskeøl', 11)
+insert into ProductCategoryProduct values ('Flaskeøl', 12)
+insert into ProductCategoryProduct values ('Flaskeøl', 13)
+
+insert into ProductCategoryProduct values ('Fadøl', 14)
+insert into ProductCategoryProduct values ('Fadøl', 15)
+insert into ProductCategoryProduct values ('Fadøl', 16)
+insert into ProductCategoryProduct values ('Fadøl', 17)
+insert into ProductCategoryProduct values ('Fadøl', 18)
+insert into ProductCategoryProduct values ('Fadøl', 19)
+insert into ProductCategoryProduct values ('Fadøl', 20)
+insert into ProductCategoryProduct values ('Fadøl', 21)
+insert into ProductCategoryProduct values ('Fadøl', 22)
+insert into ProductCategoryProduct values ('Fadøl', 23)
+insert into ProductCategoryProduct values ('Fadøl', 24)
+insert into ProductCategoryProduct values ('Fadøl', 25)
+insert into ProductCategoryProduct values ('Fadøl', 26)
+
+insert into ProductCategoryProduct values ('Fustage', 27)
+insert into ProductCategoryProduct values ('Fustage', 28)
+insert into ProductCategoryProduct values ('Fustage', 29)
+insert into ProductCategoryProduct values ('Fustage', 30)
+insert into ProductCategoryProduct values ('Fustage', 31)
+insert into ProductCategoryProduct values ('Fustage', 32)
+insert into ProductCategoryProduct values ('Fustage', 33)
+insert into ProductCategoryProduct values ('Fustage', 34)
+insert into ProductCategoryProduct values ('Fustage', 35)
+insert into ProductCategoryProduct values ('Fustage', 36)
+insert into ProductCategoryProduct values ('Fustage', 37)
+insert into ProductCategoryProduct values ('Fustage', 38)
+insert into ProductCategoryProduct values ('Fustage', 39)
+
+insert into Situation values ('Fredagsbar')
+insert into Situation values ('Standard')
+
+insert into Price values (70,0,1,'Fredagsbar')
+insert into Price values (70,0,2,'Fredagsbar')
+insert into Price values (70,0,3,'Fredagsbar')
+insert into Price values (70,0,4,'Fredagsbar')
+insert into Price values (70,0,5,'Fredagsbar')
+insert into Price values (70,0,6,'Fredagsbar')
+insert into Price values (70,0,7,'Fredagsbar')
+insert into Price values (70,0,8,'Fredagsbar')
+insert into Price values (70,0,9,'Fredagsbar')
+insert into Price values (70,0,10,'Fredagsbar')
+insert into Price values (70,0,11,'Fredagsbar')
+insert into Price values (70,0,12,'Fredagsbar')
+insert into Price values (70,0,13,'Fredagsbar')
 
 
-DECLARE @cnt int = 1
--- Product
-while @cnt < 20
-BEGIN
-    INSERT Into Product
-    VALUES('Pilsner' + convert(varchar(30), @cnt),30, 40, 'Smager dejligt')
-    set @cnt = @cnt + 1
+insert into Price values (30,0,1,'Standard')
+insert into Price values (30,0,2,'Standard')
+insert into Price values (30,0,3,'Standard')
+insert into Price values (30,0,4,'Standard')
+insert into Price values (30,0,5,'Standard')
+insert into Price values (30,0,6,'Standard')
+insert into Price values (30,0,7,'Standard')
+insert into Price values (30,0,8,'Standard')
+insert into Price values (30,0,9,'Standard')
+insert into Price values (30,0,10,'Standard')
+insert into Price values (30,0,11,'Standard')
+insert into Price values (30,0,12,'Standard')
+insert into Price values (30,0,13,'Standard')
 
-END
+insert into Price values (50,0,14,'Standard')
+insert into Price values (50,0,15,'Standard')
+insert into Price values (50,0,16,'Standard')
+insert into Price values (50,0,17,'Standard')
+insert into Price values (50,0,18,'Standard')
+insert into Price values (50,0,19,'Standard')
+insert into Price values (50,0,20,'Standard')
+insert into Price values (50,0,21,'Standard')
+insert into Price values (50,0,22,'Standard')
+insert into Price values (50,0,23,'Standard')
+insert into Price values (50,0,24,'Standard')
+insert into Price values (50,0,25,'Standard')
+insert into Price values (50,0,26,'Standard')
 
--- Customer
-DECLARE @cnt int = 1
-
-while @cnt < 20
-BEGIN
-    INSERT Into Customer
-    VALUES(2189132, 'Prutskid', @cnt)
-    set @cnt = @cnt + 1
-
-END
-
--- Employee
-DECLARE @cnt int = 1
-
-while @cnt < 20
-BEGIN
-    INSERT Into Employee
-    VALUES('1231213' + convert(varchar(30), @cnt), 'Prutskid', convert(varchar(30), @cnt))
-    set @cnt = @cnt + 1
-
-END
-
-
-
---Orderline
-Set @cnt = 1
-
-while @cnt < 20
-BEGIN
-    INSERT Into OrderLine
-    VALUES(@cnt, @cnt)
-    set @cnt = @cnt + 1
-
-END
-
-SELECT * FROM Product
-
-Insert into Sale VALUES(GETDATE(),1,1)
-Insert into Sale VALUES(GETDATE(),2,2)
-Insert into Sale VALUES(GETDATE(),3,3)
-Insert into Sale VALUES(GETDATE(),4,4)
-
-DECLARE @cnt int = 1
-
-while @cnt < 4
-BEGIN
-    INSERT Into OrderLine
-    VALUES(@cnt, @cnt, @cnt, @cnt)
-    set @cnt = @cnt + 1
-
-END
-
-SELECT * FROM ProductCategory
-Insert into ProductCategory VALUES('Prut', 'prut')
-Insert into ProductCategory VALUES('skid', 'skid')
-
-Insert into ProductCategoryProduct VALUES('prut', 5)
-Insert into ProductCategoryProduct VALUES('skid', 7)
+insert into Price values (500,0,27,'Standard')
+insert into Price values (500,0,28,'Standard')
+insert into Price values (500,0,29,'Standard')
+insert into Price values (500,0,30,'Standard')
+insert into Price values (500,0,31,'Standard')
+insert into Price values (500,0,32,'Standard')
+insert into Price values (500,0,33,'Standard')
+insert into Price values (500,0,34,'Standard')
+insert into Price values (500,0,35,'Standard')
+insert into Price values (500,0,36,'Standard')
+insert into Price values (500,0,37,'Standard')
+insert into Price values (500,0,38,'Standard')
+insert into Price values (500,0,39,'Standard')
 
 
-Insert into Situation VALUES('Fredagsbar')
-Insert into Situation VALUES('Butik')
+INSERT INTO Employee Values('Peter', '20411231', '2612973311')
+INSERT INTO Employee Values('Julius', '90211231', '7012973311')
+INSERT INTO Employee Values('Magnus', '50411231', '8012973311')
+INSERT INTO Employee Values('Kristoffer', '30411231', '9012973311')
+INSERT INTO Employee Values('Hr. Vimse', '30411231', '9012973311')
+INSERT INTO Employee Values('Prut', '30411231', '9012973311')
+INSERT INTO Employee Values('Kraler', '30411231', '9012973311')
+INSERT INTO Employee Values('Vup', '30411231', '9012973311')
+INSERT INTO Employee Values('Quaxs', '30411231', '9012973311')
+INSERT INTO Employee Values('dormor', '30411231', '9012973311')
 
-insert into Price VALUES(
-    1,1,1,'Fredagsbar'
-)
-Insert into Price VALUES(2,2,2,'Butik')
+insert into Customer (personName, phoneNumber)
+values 
+('Hans Simpleton', 29562846),
+('Jens Uglemand', 30123947),
+('Troels Gulliver', 07374927),
+('Grethe Middelman', 20475618),
+('Julie Tinkermand', 10608838),
+('Frida Hertzon', 62738406),
+('Paul Unterweiss', 19275620)
+
+insert into Customer (personName, phoneNumber, cvr)
+values 
+('Asfalt A/S', 33229478, 37452494),
+('Kloakservice Jylland', 95837262, 49273846),
+('Partybus.com', 3957230, 83270932)
+
+INSERT INTO Sale VALUES('2022/11/15', 1,1)
+INSERT INTO Sale VALUES('2022/10/15', 2,2)
+INSERT INTO Sale VALUES('2022/9/15', 4,4)
+INSERT INTO Sale VALUES('2022/8/15', 5,5)
+INSERT INTO Sale VALUES('2022/7/15', 6,6)
+
+SELECT * From Employee
+
+INSERT INTO OrderLine VALUES(5, null,7,1)
+INSERT INTO OrderLine VALUES(6, 30,8,2)
+INSERT INTO OrderLine VALUES(2, 50,9,3)
+INSERT INTO OrderLine VALUES(3, 100,10,4)
+INSERT INTO OrderLine VALUES(1, 20,11,5)
+INSERT INTO OrderLine VALUES(20, 10,12,6)
+INSERT INTO OrderLine VALUES(3, 200,13,6)
+
